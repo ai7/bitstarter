2ddcf00741a06880d21524ad4d2f22912bc19757
Raymond Chi <raychi@gmail.com>
2015-09-04 13:31:20 -0700

    update to use port 8080

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var myReadFile = function(fname) {
     return data.toString();
 };
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
